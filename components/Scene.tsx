7cb66f3898ee8dfab90797a1afb355cc56183e47
EthanKuai <ethankuaiek@gmail.com>
2026-08-01 23:59:45 +0800

    chore(package): Add three.js

---
+++ components/Scene.tsx
@@ -0,0 +1,16 @@
+"use client"
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei"
+
+export default function Scene() {
+  return (
+    <Canvas>
+      <ambientLight />
+      <mesh>
+        <boxGeometry />
+        <meshStandardMaterial color="hotpink" />
+      </mesh>
+      <OrbitControls />
+    </Canvas>
+  )
+}
