6b8b6bbddcc22550ffce303eb14778078ce3a992
EthanKuai <ethankuaiek@gmail.com>
2026-08-14 01:02:30 +0800

    style: Format warnings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import "@/styles/globals.css";
 
 import type { Metadata } from "next";
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 import Footer from "@/components/footer";
 
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,26 +5,28 @@ export default function Footer() {
   return (
     <footer className="py-6 px-[5%]">
       <div className="bar">
-
-        <span className="left mx-1">
-          Ethan Kuai, CS @ Cambridge
-        </span>
+        <span className="left mx-1">Ethan Kuai, CS @ Cambridge</span>
 
         <div className="middle flex flex-wrap items-center justify-center gap-2">
-          {
-            socials.map((s: Social) => (
-              <a key={s.name} href={s.href} title={s.name} target="_blank" rel="external" className="p-5">
-                {s.icon}
-              </a>
-            ))
-          }
+          {socials.map((s: Social) => (
+            <a
+              key={s.name}
+              href={s.href}
+              title={s.name}
+              target="_blank"
+              rel="external"
+              className="p-5"
+            >
+              {s.icon}
+            </a>
+          ))}
         </div>
 
         <span className="right text-right">
-          © 2026 Ethan Kuai<br />
+          © 2026 Ethan Kuai
+          <br />
           All rights reserved
         </span>
-
       </div>
     </footer>
   );
