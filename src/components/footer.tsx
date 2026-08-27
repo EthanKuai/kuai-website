f8a6b71a35036847688eeb0e933cae86d602bcb4
EthanKuai <ethankuaiek@gmail.com>
2026-08-27 18:39:37 +0800

    style(css): Separate footer CSS

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,16 @@
 import { socials } from "@/data/socials";
 import type { Social } from "@/data/socials";
+import styles from "./footer.module.css";
 
 export default function Footer() {
   return (
     <footer className="py-6 px-[5%]">
-      <div className="bar">
+      <div className={styles.bar}>
         <span className="left mx-1">Ethan Kuai, CS @ Cambridge</span>
 
-        <div className="middle flex flex-wrap items-center justify-center gap-2">
+        <div
+          className={`${styles.middle} flex flex-wrap items-center justify-center gap-2`}
+        >
           {socials.map((s: Social) => (
             <a
               key={s.name}
