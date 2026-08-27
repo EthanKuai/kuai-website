227d37d53353e32833dbc68713d922b5eb1651de
EthanKuai <ethankuaiek@gmail.com>
2026-08-27 23:05:42 +0800

    feat(cube): Theme-aware ThreeJS cube

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,14 +1,18 @@
 "use client";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Edges, OrbitControls } from "@react-three/drei";
+import { getTheme } from "@/lib/theme";
 
 export default function Scene() {
+  const { foreground, textBackground } = getTheme();
+
   return (
     <Canvas>
-      <ambientLight />
+      <ambientLight color={"white"} intensity={1.3} />
       <mesh>
-        <boxGeometry />
-        <meshStandardMaterial color="hotpink" />
+        <boxGeometry args={[3, 3, 3]} />
+        <meshStandardMaterial color={foreground} />
+        <Edges color={textBackground} />
       </mesh>
       <OrbitControls />
     </Canvas>
